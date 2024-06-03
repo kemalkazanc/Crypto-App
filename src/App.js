5392aa699ea38d9bf856736d5e83d222f0396b46
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-06-03 16:59:46 +0200

    mise en place de Recharts.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import HeaderInfos from "./components/HeaderInfos.jsx";
+import GlobalChart from "./components/GlobalChart.jsx";
+import axios from "axios";
 
 const App = () => {
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
+      )
+      .then((res) => setCoinsData(res.data)); // Accédez aux données réelles ici
+  }, []);
+
   return (
     <div>
-      <h1>Hello ALI </h1>
+      <HeaderInfos />
+      <GlobalChart coinsData={coinsData} />
     </div>
   );
 };

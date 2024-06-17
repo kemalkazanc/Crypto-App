6b60300a8f444bc5a58c9fea6259535dae43d92f
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-06-17 16:28:06 +0200

    update

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -55,7 +55,7 @@ const TableLine = ({ coin, index }) => {
             target="_blank"
             href={
               "https://www.coingecko.com/fr/pi%C3%A8ces/" +
-              coin.id.toLowercase()
+              coin.id.toLowerCase()
             }
           >
             <img src="./assets/info-icon.svg" alt="info-icon" />
